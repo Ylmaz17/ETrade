af73d77348cdfb43da9cf485d2b5175f693ca90e
İsmail Yılmaz <iylmz1707@gmail.com>
2022-04-12 12:31:20 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,53 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            _products = new List<Product> {
+            new Product{ProductId =1, ProductName="Kalem",UnitPrice=15,Stock=10},
+            new Product{ProductId =2, ProductName="Defter",UnitPrice=10,Stock=3},
+            new Product{ProductId =3, ProductName="Silgi",UnitPrice=2,Stock=10},
+            new Product{ProductId =4, ProductName="Pc",UnitPrice=10000,Stock=120},
+            new Product{ProductId =5, ProductName="Monitör",UnitPrice=1500,Stock=110},
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            Product productToDelete = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            return _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.Stock = product.Stock;
+        }
+    }
+}
