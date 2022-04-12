c202263226d3a80a06e0a61c77985e6f7d016e49
İsmail Yılmaz <iylmz1707@gmail.com>
2022-04-12 23:44:38 +0300

    12.04.2022

--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -1,18 +1,23 @@
-﻿using System;
+﻿using Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccess.Abstract
 {
-    public interface IEntityRepository<T>
+    // Generic constraint
+    // class : Referans tip olabilir
+    // IEntity : IEntity olabilir veya implemente eden bir nesne olabilir
+    // new() : new'lenebilir olmalı
+    public interface IEntityRepository<T> where T:class,IEntity,new()
     {
-        List<T> GetAll();
-        T Get();
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
         void Add(T entity);
         void Update(T entity);
         void Delete(T entity);
-        List<T> GetAllByCategory(int categoryId);
     }
 }
---
+++ DataAccess/Concrete/EntityFramework/ETradeContext.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    // Context : Db tablolorı ile projenin claslarını bağlama işlemi
+    public class ETradeContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-MRUSB36;Database=ETrade;Trusted_Connection=true");
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
