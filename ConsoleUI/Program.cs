78577327abf7de5658f573239ae9bbfbbbea6b30
İsmail Yılmaz <iylmz1707@gmail.com>
2022-04-13 17:38:38 +0300

    13.04.2022 (2)

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -25,9 +25,19 @@ namespace ConsoleUI
         private static void ProductTest()
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var product in productManager.GetProductDetails())
+
+            var result = productManager.GetProductDetails();
+
+            if (result.Success==true)
             {
-                Console.WriteLine(product.ProductName + "---------------------" + product.CategoryName1);
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName + "---------------------" + product.CategoryName1);
+                }
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
             }
         }
     }
---
+++ Core/Utilities/Results/Concrete/Result.cs
@@ -0,0 +1,26 @@
+﻿using Core.Utilities.Results.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results.Concrete
+{
+    public class Result : IResult
+    {
+        // Normalde set edilemez ama constructor içinde set işlemi yapılabilir
+        public Result(bool success, string message):this(success)
+        {
+            Message = message;
+        }
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
