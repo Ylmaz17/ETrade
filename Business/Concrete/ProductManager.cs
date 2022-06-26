977a9e5f502183b2354520fe80be60b338f2a6d0
İsmail Yılmaz <iylmz1707@gmail.com>
2022-06-27 00:18:36 +0300

    27.06.2022 Gün Sonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,11 +1,15 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results.Abstract;
 using Core.Utilities.Results.Concrete;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,14 +26,9 @@ namespace Business.Concrete
         {
             _productDal = productDal;
         }
-
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
-            // Business codes
-            if (product.ProductName.Length<2)
-            {
-                return new ErrorResult(Messages.ProductAdded);
-            }
             _productDal.Add(product);
             return new SuccessResult(Messages.ProductAdded);
         }
